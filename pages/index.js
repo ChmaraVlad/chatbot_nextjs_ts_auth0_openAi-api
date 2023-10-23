cd34071c95c4d30f496ad20efcfa1e6195711f4f
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-23 17:49:26 +0300

    added auth and basic mark up for index and app

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,59 @@
 import Head from "next/head";
+import Link from "next/link";
+
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+
+  if (isLoading) return <div>...Loading</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <div>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Chatty Pete - Login or Signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!!user && <Link href="/api/auth/logout"> Logout</Link>}
+          {!user && (
+            <>
+              <Link
+                href="/api/auth/login"
+                className="btn"
+              >
+                {" "}
+                Login
+              </Link>
+              <Link
+                href="/api/auth/signup"
+                className="btn"
+              >
+                {" "}
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context.req, context.res);
+
+  if (!!session) {
+    return {
+      redirect: {
+        destination: "/chat",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
