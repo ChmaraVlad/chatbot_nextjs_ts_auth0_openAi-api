b1e0c005cf24dd807367e8b6a3ad47890294b01a
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-24 12:36:56 +0300

    added api createNewChat logic

---
+++ pages/api/chat/createNewChat.js
@@ -0,0 +1,32 @@
+import { getSession } from "@auth0/nextjs-auth0";
+import clientPromise from "lib/mongodb";
+
+
+export default async function handler(req, res) {
+    try {
+        const {user} = await getSession(req, res)
+        const { message } = JSON.parse(req.body); 
+
+        const newUserMessage = {
+            role: 'user',
+            content: message
+        }
+        const client = await clientPromise
+
+        const db = client.db('ChattyPete')
+        const chat = await db.collection("chats").insertOne({
+          userId: user.sub,
+          messages: [newUserMessage],
+          title: message,
+        });
+
+        res.status(200).json({
+            _id: chat.insertedId.toString(),
+            messages: [newUserMessage],
+            title: message
+        })
+    } catch (error) {
+        console.log('ERROR OCCURED IN CREATE NEW CHAT => ERROR: ', error);
+        res.status(500).json({message: 'ERROR OCCURED WHEN CREATING A NEW CHAT'})
+    }
+}
