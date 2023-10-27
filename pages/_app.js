c9e367223b5fb60aaaf98156cbfc9622d6352dc1
Vlad Chmara <chmaravlad5@gmail.com>
2023-10-28 00:06:10 +0300

    added google font for body from next/font/google

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,31 @@
 import Head from "next/head";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
+import { Outfit } from "next/font/google";
+
 import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css"
-config.autoAddCss = false
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import "../styles/globals.css";
+
+config.autoAddCss = false;
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+});
 
 function App({ Component, pageProps }) {
   return (
     <>
-    <UserProvider>
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <Component {...pageProps} />
-    </UserProvider>
+      <UserProvider>
+        <Head>
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <main className={`${outfit.variable} font-body`}>
+          <Component {...pageProps} />
+        </main>
+      </UserProvider>
     </>
   );
 }
